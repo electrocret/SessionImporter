a9c1b9f81da3a50dbb41b3d9a71b03407544eb47
electrocret <monkeybrains7@gmail.com>
2022-08-29 12:20:08 -0500

    Basic LibreNMS example

---
+++ Example/Librenms.py
@@ -0,0 +1,12 @@
+from LibreNMSAPIClient import LibreNMSAPIClient #LibreNMSAPIClient https://github.com/electrocret/LibreNMSAPIClient
+from SessionImporter import SessionImporter,Session,Folder
+
+libreapi =  LibreNMSAPIClient()
+importer = SessionImporter()
+
+devices = libreapi.list_devices()
+
+for device in devices
+	importer.add(Session(device.hostname,device.ip))
+
+importer.write_securecrt("Librenms.xml")
